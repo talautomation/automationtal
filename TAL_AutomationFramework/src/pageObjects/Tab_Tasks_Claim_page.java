21aba442c4f12f034e933ee23bdb170b2d806826
sindhuhs <sindhu.hs@fineos.com>
2016-01-29 09:16:57 +1100

    Tasks Claim Page_css path updated

--- TAL_AutomationFramework/src/pageObjects/Tab_Tasks_Claim_page.java
+++ TAL_AutomationFramework/src/pageObjects/Tab_Tasks_Claim_page.java
@@ -5,33 +5,36 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class Tab_Tasks_Claim_page {
-	
-	
+
 	private static WebElement element = null;
-	
-	public static WebElement tab_task(WebDriver driver){
-	   element = driver.findElement(By.xpath(".//*[@id='DisplayCaseTabbedDialogWidget_un22_CaseTabControlBean_Tasks_cell']/div"));
-	   utility.Waitutil.implicitwait(driver);
-	   return element;
-	   }
-	 
-	public static WebElement tbl_TasksList(WebDriver driver){
+
+	public static WebElement tab_task(WebDriver driver) {
+		// element =
+		// driver.findElement(By.xpath(".//*[@id='DisplayCaseTabbedDialogWidget_un22_CaseTabControlBean_Tasks_cell']/div"));
+		element = driver.findElement(
+				By.cssSelector("*[id^='DisplayCaseTabbedDialogWidget_'][id$='_CaseTabControlBean_Tasks_cell']"));
+		utility.Waitutil.implicitwait(driver);
+		return element;
+	}
+
+	public static WebElement tbl_TasksList(WebDriver driver) {
 		element = driver.findElement(By.cssSelector("*[id^='TasksForCaseListViewWidget_'][id$='_TasksForCaseWidget']"));
 		utility.Waitutil.implicitwait(driver);
 		return element;
 	}
-	 
-	//public static WebElement row_righttask(WebDriver driver){
-		//element = driver.findElement(By.cssSelector("*[id^='TasksForCaseListViewWidget_'][id$='_TasksForCaseWidget']"));
-		//utility.Waitutil.implicitwait(driver);
-		//return element;
-	//}	
-	
-	public static WebElement btn_Closetask(WebDriver driver){
-	
+
+	// public static WebElement row_righttask(WebDriver driver){
+	// element =
+	// driver.findElement(By.cssSelector("*[id^='TasksForCaseListViewWidget_'][id$='_TasksForCaseWidget']"));
+	// utility.Waitutil.implicitwait(driver);
+	// return element;
+	// }
+
+	public static WebElement btn_Closetask(WebDriver driver) {
+
 		element = driver.findElement(By.cssSelector("*[id^='TasksForCaseListViewWidget_'][id$='_closeTask-button']"));
 		utility.Waitutil.implicitwait(driver);
 		return element;
-	}	
+	}
 
 }
